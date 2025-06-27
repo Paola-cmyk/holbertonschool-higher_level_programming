8af75e7ba32f8b2e71826a2f31b17983370f07d2
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-06-27 13:50:07 -0400

    2nd 2nd

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,16 +1,5 @@
 --create table second_table in hbtn_0c_0 in MySQL server
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT PRIMARY KEY,
-    name VARCHAR(256),
-    score INT
-    );
-
-INSERT INTO
-    second_table (id, name, score)
-VALUES (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8)
-ON DUPLICATE KEY UPDATE
-    name = VALUES(name),
-    score = VALUES(score);
+CREATE TABLE IF NOT EXISTS second_table 
+(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) 
+VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
