def71ab54ffd2225854ca975da11e3658d75d5af
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-06-27 14:26:21 -0400

    L

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,5 +1,6 @@
 --lists all records with a score from hbtn_0c_0 in MySQL server
-SELECT score, name 
-FROM second_table 
-WHERE score >= 10 
-ORDER BY score DESC;
+SELECT score, name
+FROM second_table
+WHERE
+    score >= 10
+ORDER BY score DESC
